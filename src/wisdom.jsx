86ced1d81ccf0f59623efc38c686a8f7fc1f6d8f
RoboticsDevelopmentName <RoboticsDevelopment@protonmail.com>
2023-09-10 12:01:09 -0400

    Mission Complete

---
+++ src/wisdom.jsx
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+import axios from 'axios'
+
+
+function Wisdom() {
+      const [quote, setQuote] = useState('')
+      const getQuote = () => {
+        axios.get('https://api.quotable.io/random')
+        .then(res => {
+          console.log(res.data.content)
+          setQuote(res.data.content)
+        }).catch(err => {
+        console.log(err)  
+        })
+  }
+  return (
+    <div>
+      <button onClick={getQuote}>Get Quote</button>
+      {quote && <p>{quote}</p>}
+    </div>
+  );
+  }
+  
+  
+  export default Wisdom;
