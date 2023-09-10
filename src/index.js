c9653750acf01312356b97e2a34d77d881d61ca1
RoboticsDevelopmentName <RoboticsDevelopment@protonmail.com>
2023-09-10 12:57:36 -0400

    Update 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Wisdom from './wisdom';
-import App from './App';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<div>
- <App/>
+
   <Wisdom/>
 
-</div>
+
  
 
 );
--- src/wisdom.jsx
+++ src/wisdom.jsx
@@ -1,25 +1,26 @@
-import {useState} from 'react'
-import axios from 'axios'
-
+import { useState } from "react";
+import axios from "axios";
 
 function Wisdom() {
-      const [quote, setQuote] = useState('')
-      const getQuote = () => {
-        axios.get('https://api.quotable.io/random')
-        .then(res => {
-          console.log(res.data.content)
-          setQuote(res.data.content)
-        }).catch(err => {
-        console.log(err)  
-        })
-  }
+  const [quote, setQuote] = useState("");
+  const getQuote = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((res) => {
+        console.log(res.data.content);
+        setQuote(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
+      <h2 style={{ textAlign: "Center" }}>Words of Wisdom</h2>
       <button onClick={getQuote}>Get Quote</button>
       {quote && <p>{quote}</p>}
     </div>
   );
-  }
-  
-  
-  export default Wisdom;
+}
+
+export default Wisdom;
